53ec72577511f042ae126bf568348e3f5e82b39e
Rihards Gravis <rihards@gravis.lv>
2024-01-19 15:22:15 +0200

    Update base Docker image

--- deploy-docker/actions.py
+++ deploy-docker/actions.py
@@ -1,29 +1,50 @@
+"""
+A simple AI Action template for comparing timezones
 
-import json
+Please checkout the base guidance on AI Actions in our main repository readme:
+https://github.com/robocorp/robocorp/blob/master/README.md
+
+"""
+
 from robocorp.actions import action
-from robocorp import browser
+from datetime import datetime
+import pytz
+
 
 @action
-def search_wikipedia(topic: str) -> str:
-    """Search Wikipedia for a topic
+def compare_time_zones(user_timezone: str, compare_to_timezones: str) -> str:
+    """
+    Compares user timezone time difference to given timezones
 
     Args:
-        topic (str): Topic to search in Wikipedia
+        user_timezone (str): User timezone in tz database format. Example: "Europe/Helsinki"
+        compare_to_timezones (str): Comma seperated timezones in tz database format. Example: "America/New_York, Asia/Kolkata"
 
     Returns:
-        str: First paragraph of the topic wiki page
+        str: List of requested timezones, their current time and the user time difference in hours
     """
+    output: list[str] = []
 
-    browser.configure(browser_engine="chromium", headless=True)
-    page = browser.goto("https://wikipedia.org")
+    try:
+        user_tz = pytz.timezone(user_timezone)
+        user_now = datetime.now(user_tz)
+    except pytz.InvalidTimeError:
+        return f"Timezone '{user_timezone}' could not be found. Use tz database format."
+    
+    output.append(f"- Current time in {user_timezone} is {user_now.strftime('%I:%M %p')}")
 
+    target_timezones = [s.strip() for s in compare_to_timezones.split(',')]
+    for timezone in target_timezones:
+        try:
+            target_tz = pytz.timezone(timezone)
+            target_now = datetime.now(target_tz)
+            time_diff = (int(user_now.strftime('%z')) - int(target_now.strftime('%z'))) / 100
 
-    page.fill("#searchInput", f"{topic}")
-    page.press("#searchInput", "Enter")
+            output.append(f"- Current time in {timezone} is {target_now.strftime('%I:%M %p')}, the difference with {user_timezone} is {time_diff} hours")
+        except pytz.InvalidTimeError:
+            output.append(f"- Timezone '{timezone}' could not be found. Use tz database format.")
 
-    page.wait_for_selector("#mw-content-text")
-
-    paragraph = page.query_selector("#mw-content-text > div p:not(.mw-empty-elt)")
-    content = paragraph.text_content()
-
-    return content
+    # Pretty print for log
+    print("\n".join(output))
+    
+    return "\n".join(output)
