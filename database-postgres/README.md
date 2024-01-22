0f5e1225d27791f0efbc28d1e29e759d8303054a
Tommi Holmgren <tommi@robocorp.com>
2024-01-22 16:24:46 +0200

    Added database example

---
+++ database-postgres/README.md
@@ -0,0 +1,43 @@
+# Example: Actions - Sales results & Computer uptime
+
+This example gets you started in creating Actions for [Robocorp Action Server](https://github.com/robocorp/robo/tree/master/action_server/docs#readme).
+
+`Actions` and `Action Server` enable you to "give your AI Agents hands" meaning that your AI/LLM Agent can help your users perform distinct actions that get executed based on the LLM discussion.
+
+The example shows how to let your LLM app (like custom GPT) access a Postgres database, but only with queries that you have determined.
+
+## What does this example Action do?
+
+This has four Actions:
+1. List customers by first name
+2. List customers latest rental transactions
+3. List availabitilty of a movie in different stores by the title
+4. Update customers email address
+
+Actions 1-3 are configured to be `is_consequential=False`, which means that when used with OpenAI custom GPTs, the user can allow the actions to run without explicit permission request every time. The last action that has a consequence on the actual data, is set to always require user consent to run.
+
+## Quickstart
+
+👉 Follow the Action Server [Quickstart guide](https://github.com/robocorp/robocorp?tab=readme-ov-file#%EF%B8%8F-quickstart) in the main repository.
+
+👉 Set up your local Postgres server and set it up with the [DVD Rental example data](https://www.postgresqltutorial.com/postgresql-getting-started/postgresql-sample-database/). Follow the instructions from the given site.
+
+
+## Dependency management
+
+We recommend placing your dependencies in [conda.yaml](conda.yaml).
+
+👉 More on [managing your dependencies](https://github.com/robocorp/robocorp?tab=readme-ov-file#what-makes-a-python-function-an-%EF%B8%8Faction) in the main repository.
+
+
+## Actions in VS Code
+
+👉 Using [Robocorp Code extension for VS Code](https://marketplace.visualstudio.com/items?itemName=robocorp.robocorp-code), you can get everything set up and running in VS Code in no time.
+
+The template has a few files that enable the extension to find and set up your action environment and provide code completion. There is also a side panel where we have and will add some easy-to-use functionalities.
+
+When debugging your Actions Python code, you probably do not want to give the inputs every time you run and always be running the Action Server, so you can set your test inputs in [devdata](./devdata) folder and just run/debug your Python code.
+
+
+🚀 Now, go get'em
+
---
+++ database-postgres/actions.py
@@ -0,0 +1,196 @@
+"""
+AI Actions for dvdrental PostgreSQL database queries.
+"""
+
+import psycopg2
+from robocorp.actions import action
+
+
+@action(is_consequential=False)
+def customers_by_name(first_name: str) -> str:
+    """
+    List customers by first name.
+
+    Args:
+        first_name (str): Customer's first name in string format. Example: "Jill"
+
+    Returns:
+        str: List of customers based on the first name (customer number, first name, last name, email address).
+    """
+
+    # Use some .env or vault rather than adding your password here.
+    params = {
+        'database': 'dvdrental',
+        'user': 'YOUR_USERNAME',
+        'password': 'YOUR_PASSWORD',
+        'host': 'localhost',
+        'port': '5432'
+    }
+
+    conn = psycopg2.connect(**params)
+    cur = conn.cursor()
+
+    sql_query = """
+    SELECT customer_id, first_name, last_name, email
+    FROM customer
+    WHERE first_name = %s;
+    """
+    cur.execute(sql_query, (first_name,))
+    rows = cur.fetchall()
+
+    output = []
+    for c_num, fn, ln, email in rows:
+        output.append(f"ID: {c_num}, {fn}, {ln}, {email}")
+
+    output_str = "\n".join(output)
+    print(output_str)
+    cur.close()
+    conn.close()
+    return output_str
+
+
+@action(is_consequential=False)
+def customers_rentals(first_name: str, last_name: str, limit: int = 5) -> str:
+    """
+    List customers latest rental transactions.
+
+    Args:
+        first_name (str): Customer's first name in string format. Example: "Jill"
+        last_name (str): Customer's last name in string format. Example: "Jones"
+        limit (int): Number of results to query. Example: 5
+
+    Returns:
+        str: List of customers latest rental transactions containing rental date and the movie title.
+    """
+
+    # Use some .env or vault rather than adding your password here.
+    params = {
+        'database': 'dvdrental',
+        'user': 'YOUR_USERNAME',
+        'password': 'YOUR_PASSWORD',
+        'host': 'localhost',
+        'port': '5432'
+    }
+
+    conn = psycopg2.connect(**params)
+    cur = conn.cursor()
+
+    sql_query = """
+    SELECT r.rental_date, f.title AS movie_title
+    FROM rental r
+    JOIN customer c ON r.customer_id = c.customer_id
+    JOIN inventory i ON r.inventory_id = i.inventory_id
+    JOIN film f ON i.film_id = f.film_id
+    WHERE c.first_name = %s AND c.last_name = %s
+    ORDER BY rental_date DESC
+    LIMIT %s;
+    """
+
+    cur.execute(sql_query, (first_name, last_name, limit))
+    rows = cur.fetchall()
+
+    output = []
+    for rental_date, movie_title in rows:
+        formatted_date = rental_date.strftime("%Y-%m-%d %H:%M:%S")
+        output.append(f"{formatted_date}, {movie_title}")
+
+    output_str = "\n".join(output)
+    cur.close()
+    conn.close()
+    return output_str
+
+
+@action(is_consequential=False)
+def availability_in_stores(movie_title: str) -> str:
+    """
+    List availabitilty of a movie in different stores by the title.
+
+    Args:
+        title (str): Movie title. Example: "League Hellfighters"
+
+    Returns:
+        str: List of stores where the movie is available.
+    """
+
+    # Use some .env or vault rather than adding your password here.
+    params = {
+        'database': 'dvdrental',
+        'user': 'YOUR_USERNAME',
+        'password': 'YOUR_PASSWORD',
+        'host': 'localhost',
+        'port': '5432'
+    }
+
+    conn = psycopg2.connect(**params)
+    cur = conn.cursor()
+    limit = 5
+
+    sql_query = """
+    SELECT DISTINCT s.store_id, a.address, a.district, ci.city, co.country
+    FROM film f
+    JOIN inventory i ON f.film_id = i.film_id
+    JOIN store s ON i.store_id = s.store_id
+    JOIN address a ON s.address_id = a.address_id
+    JOIN city ci ON a.city_id = ci.city_id
+    JOIN country co ON ci.country_id = co.country_id
+    WHERE f.title = %s
+    LIMIT %s;
+    """
+
+    cur.execute(sql_query, (movie_title, limit))
+    rows = cur.fetchall()
+    output = []
+    for row in rows:
+        output.append(f"Store ID: {row[0]}, Address: {row[1]}, {row[2]}, {row[3]}, {row[4]}")
+
+    output_str = "\n".join(output)
+    print(output_str)
+    cur.close()
+    conn.close()
+    return output_str
+
+
+@action
+def update_customer(id: int, new_email: str) -> str:
+    """
+    Update customers email address.
+
+    Args:
+        id (str): Customer ID. Example: 123
+        new_email (str): New email address. Example: "first.last@example.com
+
+    Returns:
+        str: Information whether the update was successful.
+    """
+
+    # Use some .env or vault rather than adding your password here.
+    params = {
+        'database': 'dvdrental',
+        'user': 'YOUR_USERNAME',
+        'password': 'YOUR_PASSWORD',
+        'host': 'localhost',
+        'port': '5432'
+    }
+
+    conn = psycopg2.connect(**params)
+    cur = conn.cursor()
+
+    sql_query = """
+    UPDATE customer
+    SET email = %s
+    WHERE customer_id = %s;
+    """
+
+    cur.execute(sql_query, (new_email, id))
+
+    conn.commit()
+    if cur.rowcount == 1:
+        print(f"Email updated successfully for customer ID {id}.")
+        output = f"Email updated successfully for customer ID {id}."
+    else:
+        print(f"No record found for customer ID {id}.")
+        output = f"No record found for customer ID {id}."
+
+    cur.close()
+    conn.close()
+    return output
