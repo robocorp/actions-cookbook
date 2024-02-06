63e856073fbe3567a13247d98dfa4f1691196eba
Mikko Korpela <mikko@robocorp.com>
2024-02-06 13:51:01 +0200

    add initial example-postgres-universal

---
+++ example-postgres-universal/README.md
@@ -0,0 +1,10 @@
+# PostgreSQL Universal 
+
+
+To run this use `action-server start`.
+
+You may then check http://localhost:8080 for the Action Server UI.
+
+You can expose this action to internet with `action-server start --expose`.
+Then you may use it in services such as OpenAI GPT Action.
+
---
+++ example-postgres-universal/action.py
@@ -0,0 +1,14 @@
+from robocorp.actions import action
+
+
+@action
+def greet(name: str) -> str:
+    """Produces an excellent greeting for the given person
+
+    Args:
+        name (str): Name of the person to greet.
+
+    Returns:
+        str: An excellent greeting
+    """
+    return f"Hello {name}!"
