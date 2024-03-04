c5d4d92d447f05e596542b69e83b37311115a1a0
Kari Harju <kari@robocorp.com>
2024-03-04 19:11:01 +0200

    Harmonized all the deploy examples to use the same action package content
    - Added devdata
    - Added missing .gitignore
    - 'actions.py' instead of 'action.py'

--- deploy-fly-io/actions.py
+++ deploy-fly-io/actions.py
@@ -7,8 +7,10 @@ https://github.com/robocorp/robocorp/blob/master/README.md
 """
 
 from robocorp.actions import action
-from datetime import datetime
+
 import pytz
+from datetime import datetime
+from zoneinfo import ZoneInfo
 
 
 @action
@@ -31,18 +33,28 @@ def compare_time_zones(user_timezone: str, compare_to_timezones: str) -> str:
     except pytz.InvalidTimeError:
         return f"Timezone '{user_timezone}' could not be found. Use tz database format."
     
-    output.append(f"- Current time in {user_timezone} is {user_now.strftime('%I:%M %p')}")
+    output.append(
+        f"- Current time in {user_timezone} is {user_now.strftime('%I:%M %p')}"
+    )
 
-    target_timezones = [s.strip() for s in compare_to_timezones.split(',')]
+    target_timezones = [s.strip() for s in compare_to_timezones.split(",")]
     for timezone in target_timezones:
         try:
             target_tz = pytz.timezone(timezone)
             target_now = datetime.now(target_tz)
-            time_diff = (int(user_now.strftime('%z')) - int(target_now.strftime('%z'))) / 100
+            user_offset, target_offset = (
+                user_now.astimezone(ZoneInfo(user_timezone)).utcoffset(),
+                user_now.astimezone(ZoneInfo(timezone)).utcoffset(),
+            )
+            time_diff = (user_offset - target_offset).total_seconds() / 3600
 
-            output.append(f"- Current time in {timezone} is {target_now.strftime('%I:%M %p')}, the difference with {user_timezone} is {time_diff} hours")
+            output.append(
+                f"- Current time in {timezone} is {target_now.strftime('%I:%M %p')}, the difference with {user_timezone} is {time_diff} hours"
+            )
         except pytz.InvalidTimeError:
-            output.append(f"- Timezone '{timezone}' could not be found. Use tz database format.")
+            output.append(
+                f"- Timezone '{timezone}' could not be found. Use tz database format."
+            )
 
     # Pretty print for log
     print("\n".join(output))
